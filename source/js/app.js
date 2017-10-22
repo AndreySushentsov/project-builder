680eae0ec5a6f9914252cc4adc6caae7d35bd155
andrey sushentsov <p1oneer8527@gmail.com>
2017-10-22 13:48:49 +0500

    mega commit

--- source/js/app.js
+++ source/js/app.js
@@ -1,4 +1,11 @@
 (function() {
   'use strict';
+  // var rowLength = $('.product__row').length;
+  // var rowNumber = $('.product__row:eq(0)').find('.product__card');
+  // rowNumber.addClass('product__card_right');
+  //
+  // for(var i = 1; i<=rowLength; i++){
+  //   if()
+  // }
 
 })();
