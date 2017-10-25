0f9e6ae0c82523c3c9f8e57d3723dd3476006f9b
Andrey Sushentsov <p1oneer8527@gmail.com>
2017-10-25 16:11:35 +0500

    finish i hope

--- source/js/app.js
+++ source/js/app.js
@@ -9,6 +9,32 @@
   // }
     $('a[data-rel^=lightcase]').lightcase();
 
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+      loop:true,
+      autoplay:true,
+      responsive:{
+        0:{
+          items:1
+        },
+        600:{
+          items:2
+        },
+        998:{
+          items:3
+        }
+      }
+    });
+
+    $('#control_l').on('click', function(e){
+      e.preventDefault();
+      owl.trigger('prev.owl.carousel');
+    });
+    $('#control_r').on('click', function(e){
+      e.preventDefault();
+      owl.trigger('next.owl.carousel');
+    });
+
     $('.modal__close-btn').on('click',function(e) {
         e.preventDefault();
         $('.modal__window').fadeOut('fast');
@@ -20,5 +46,12 @@
     })
     $('.button_disign').on('click', function functionName() {
       $('.modal__window_quickorder').fadeIn('fast');
+    })
+    $('.main-baner__button_slider').on('click',function () {
+      $('.modal__window').fadeIn('fast');
+    })
+    $('.object__title a').on('click',function (e) {
+      e.preventDefault();
+      $('.modal__window_product').fadeIn('fast');
     })
 })();
